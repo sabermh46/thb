8d3110e52f4ae36aa62faa03d63b47f22694e908
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2023-06-14 01:58:17 +0600

    Update

--- script.js
+++ script.js
@@ -30,20 +30,118 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
     const doorGallery = document.querySelector('.doorGallery');
-
-    const doors = doorGallery.querySelectorAll('.door')
+    const doors = doorGallery.querySelectorAll('.door');
+    const indicator = document.querySelector('.door_indicator');
 
     var doorIndex = 0
-    var maxIndex = doors.length
-    
-    console.log(maxIndex);
-    console.log(doors);
+    var maxIndex = doors.length - 1
 
-    function openTheDoor(index) {
-        doors[index].classList.add('active')
-        console.log(doors[index]);
+    doors.forEach((item, i)=>{
+        var div = document.createElement('div');
+        div.classList.add('item')
+        indicator.append(div)
+      })
+
+      const indicators = document.querySelectorAll('.door_indicator .item');
+      indicators.forEach((item, i)=>{
+        item.addEventListener('click', ()=>{
+            goToIndex(i)
+        })
+      })
+
+    function openTheDoor(e) {
+        e.classList.add('active');
+    }
+    function closeTheDoor(e) {
+        e.classList.remove('active')
     }
 
-    openTheDoor(doorIndex)
+    function nextIndex(){
+        goToIndex(++doorIndex)
+    }
+
+
+    function goToIndex(index){
+        if(index > maxIndex){
+            goToIndex(0)
+        } else if(index < 0){
+            goToIndex(maxIndex)
+        } else {
+            doors.forEach(door=>{
+                closeTheDoor(door)
+            })
+            indicators.forEach(item=>{
+                item.classList.remove('active')
+            })
+
+            doorIndex = index
+            openTheDoor(doors[index])
+            indicators[index].classList.add('active')
+
+
+            stopTimer()
+            startTimer()
+        }
+    }
+
+
+    var timer;
+  
+    function startTimer(){
+      timer = setInterval(()=>{
+        nextIndex()
+      }, 3000)
+    }
+    function stopTimer(){
+      clearInterval(timer)
+    }
+
+    goToIndex(doorIndex)
+
+
+    class ActivateInView {
+        constructor(element) {
+            this.element = element;
+            this.init();
+        }
+    
+        init() {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        this.onInView(entry.target);
+                        observer.unobserve(entry.target);
+                    }
+                });
+            });
+    
+            observer.observe(this.element);
+        }
+    
+        onInView(element) {
+            element.classList.add('active');
+        }
+    }
+    
+
+    
+    var fromRightToLeft_s = document.querySelectorAll('.fromRightToLeft')
+
+    fromRightToLeft_s.forEach(item=>{
+        new ActivateInView(item)
+    })
+    
+    var fromLeftToRight_s = document.querySelectorAll('.fromLeftToRight')
+    
+    
+    fromLeftToRight_s.forEach(item=>{
+        new ActivateInView(item)
+    })
+    
+    var fromDownToTop_s = document.querySelectorAll('.fromDownToTop')
+    
+    fromDownToTop_s.forEach(item=>{
+        new ActivateInView(item)
+    })
 
 })
